182fa51b727d1037450a7b70fe35bc3ec999b3ad
Gui-dev <adrianon10@gmail.com>
2021-08-23 17:57:33 -0300

    feat: add config leaflet

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,9 +10,14 @@ function MyApp ({ Component, pageProps }: AppProps) {
         <link rel="shortcut icon" href="/images/icon-512.png"/>
         <link rel="apple-touch-icon" href="/images/icon-512.png"/>
         <link rel="manifest" href="/manifest.json"/>
-        <meta name="theme-color" content="#06092B"/>
-        <title>My Trips</title>
         <meta name="description" content="A simple project to show my favorites spots in the world"/>
+        <meta name="theme-color" content="#06092B"/>
+
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+        integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+        crossOrigin="true"/>
+
+        <title>My Trips</title>
       </Head>
       <GlobalStyle />
       <Component {...pageProps} />
