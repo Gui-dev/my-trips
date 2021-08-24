83a682b59d0c8217001b75334f41b10aa67f97d7
Gui-dev <adrianon10@gmail.com>
2021-08-24 03:32:29 -0300

    feat: add property MapProps

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -2,18 +2,40 @@ import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 
 // import { Container, Description, Illustration, Logo, Title } from './style'
 
-const Map = () => {
+type Place = {
+  id: string
+  name: string
+  slug: string
+  location: {
+    latitude: number
+    longitude: number
+  }
+}
+
+type MapProps = {
+  places?: Place[]
+}
+
+const Map = ({ places }: MapProps) => {
   return (
     <MapContainer center={[0, 0]} zoom={3} style={{ height: '100%', width: '100%' }}>
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[0, 0]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      { places?.map(place => {
+        return (
+          <Marker
+            key={String(place.id)}
+            position={[place.location.latitude, place.location.longitude]}
+            title={place.name}
+          >
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        )
+      }) }
     </MapContainer>
   )
 }
