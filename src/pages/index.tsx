d61c8328e98cbefcf625638035b6a19e2e7e58d1
Gui-dev <adrianon10@gmail.com>
2021-08-24 20:24:34 -0300

    feat: add LinkWrapper component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,27 @@
 import dynamic from 'next/dynamic'
+import { InfoOutline } from '@styled-icons/evaicons-outline/InfoOutline'
+
+import { LinkWrapper } from 'components/LinkWrapper'
 
 const Map = dynamic(() => import('components/Map'), { ssr: false })
 
 export default function Home () {
+  const place = {
+    id: '1',
+    name: 'São Paulo',
+    slug: 'sao paulo',
+    location: {
+      latitude: 0,
+      longitude: 0
+    }
+  }
+
   return (
-    <Map />
+    <>
+      <LinkWrapper href="/about">
+        <InfoOutline size={32} area-label="About"/>
+      </LinkWrapper>
+      <Map places={[place]}/>
+    </>
   )
 }
